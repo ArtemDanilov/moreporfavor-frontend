61ad6937a0479118d129304ef3e9a8ff9c986748
ArtemD <artemdanilow95@gmail.com>
2023-06-08 21:06:05 +0200

    Add wrapper to header

--- src/app/layout/header/index.tsx
+++ src/app/layout/header/index.tsx
@@ -11,20 +11,22 @@ const Header = async () => {
   const links = await fetchLinks();
 
   return (
-    <header className="max-w-8xl fixed absolute-x-centered top-0 z-50 w-full py-4 px-5 bg-white flex items-center justify-between xl:px-20">
-      <h1>
-        <Link href="/">
-          <span className="sr-only">Moreporfavor Logo</span>
-          <Logo width={140} height={58} />
-        </Link>
-      </h1>
+    <header className="fixed absolute-x-centered top-0 z-50 w-full bg-white">
+      <div className="max-w-8xl mx-auto py-4 px-5 flex items-center justify-between xl:px-20">
+        <h1>
+          <Link href="/">
+            <span className="sr-only">Moreporfavor Logo</span>
+            <Logo width={140} height={58} />
+          </Link>
+        </h1>
 
-      <DesktopNav links={links} />
+        <DesktopNav links={links} />
 
-      <div className="flex items-center space-x-4">
-        <Magnifier width={24} height={24} />
-        <Sun width={24} height={24} />
-        <MobileNav links={links} />
+        <div className="flex items-center space-x-4">
+          <Magnifier width={24} height={24} />
+          <Sun width={24} height={24} />
+          <MobileNav links={links} />
+        </div>
       </div>
     </header>
   );
