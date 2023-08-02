9a86c6e85f68bab1348d90692d41cc913455a682
ArtemD <artemdanilow95@gmail.com>
2023-08-02 19:06:28 +0200

    Create aside bar to homepage

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -10,7 +10,7 @@ const Section = ({ children, heading, className = "" }: Props) => {
       className={`container mb-8 last:mb-0 md:mb-12 xl:mb-18 ${className}`}
     >
       {heading && (
-        <h2 className="text-sans text-2xl font-bold text-center text-green mb-5">
+        <h2 className="text-sans text-2xl font-bold text-center text-green mb-5 md:text-4xl md:mb-8 xl:text-5xl xl:mb-12">
           {heading}
         </h2>
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import "./style.scss";
+
 import Hero from "./components/hero/homepage/Hero";
 import About from "./components/sections/About";
 
@@ -12,17 +14,23 @@ const Home = async () => {
     <>
       <Hero entries={allPosts} />
 
-      <Section heading="Krótko o mnie">
-        <About
-          img="/assets/me.jpg"
-          text="Lorem ipsum dolor sit amet consectetur. Blandit euismod cras orci enim gravida tellus ultricies tincidunt. Sit felis in vitae eu augue pretium nunc. Aliquam elit vitae congue aenean. Proin arcu vulputate sem purus morbi urna. Pulvinar suspendisse eget amet in id in aliquet."
-          className="sm:flex sm:items-center sm:space-x-5 sm:max-w-[37.75rem] sm:text-left"
-        />
-      </Section>
+      <div className="xl:flex xl:flex-row-reverse xl:justify-center xl:gap-x-4">
+        <aside className="space-y-8 mb-8 md:mb-12 xl:flex-[30%] xl:mb-0 xl:max-w-xs-2">
+          <Section heading="Krótko o mnie">
+            <About
+              img="/assets/me.jpg"
+              text="Lorem ipsum dolor sit amet consectetur. Blandit euismod cras orci enim gravida tellus ultricies tincidunt. Sit felis in vitae eu augue pretium nunc. Aliquam elit vitae congue aenean. Proin arcu vulputate sem purus morbi urna. Pulvinar suspendisse eget amet in id in aliquet."
+              className="sm:flex sm:items-center sm:space-x-5 sm:max-w-[37.75rem] sm:text-left xl:block xl:space-x-0 xl:text-center"
+            />
+          </Section>
+        </aside>
 
-      <Section heading="Najnowsze artykuły">
-        <BlogPosts posts={allPosts} />
-      </Section>
+        <div className="xl:flex-[70%] max-w-5xl">
+          <Section heading="Najnowsze artykuły">
+            <BlogPosts posts={allPosts} />
+          </Section>
+        </div>
+      </div>
     </>
   );
 };
