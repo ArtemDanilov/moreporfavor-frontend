738e2de28e6df3f444f439390a6af66e0872eeeb
ArtemD <artemdanilow95@gmail.com>
2023-08-03 12:20:39 +0200

    Remove quality attribute for article hero image

--- src/app/components/hero/article/Hero.tsx
+++ src/app/components/hero/article/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = ({ image, title }: typeHero) => {
   const imgPath = getFullImagePath(url);
 
   return (
-    <div className="relative h-[calc(100vh-5.625rem)] max-h-[52.5rem] mb-8 overflow-hidden md:mb-12 xl:mb-24">
+    <div className="relative h-[calc(100vh-5.625rem)] max-h-[52.5rem] bg-gray-200 mb-8 overflow-hidden md:mb-12 xl:mb-24">
       <h1 className="absolute absolute-centered z-30 font-display font-normal text-5xl leading-tight text-white text-center">
         {title}
       </h1>
@@ -30,7 +30,6 @@ const Hero = ({ image, title }: typeHero) => {
         priority={true}
         style={imageStyle}
         className="object-cover object-center brightness-[.6]"
-        quality={90}
       ></Image>
       <Decoration1 className="absolute z-20 bottom-0 left-0 w-full h-20 lg:h-24 2xl:h-32" />
     </div>
