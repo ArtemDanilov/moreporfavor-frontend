d21013d067c8ea09805d60b62d43e639c917a643
ArtemD <artemdanilow95@gmail.com>
2023-08-08 10:24:33 +0200

    Create svg frame components

---
+++ src/app/components/svg/Frames.tsx
@@ -0,0 +1,6 @@
+import Vintage from "./frames/Vintage";
+import Oriental from "./frames/Oriental";
+import PolishFolklor from "./frames/PolishFolklor";
+import Branches from "./frames/Branches";
+
+export { Vintage, Oriental, PolishFolklor, Branches };
