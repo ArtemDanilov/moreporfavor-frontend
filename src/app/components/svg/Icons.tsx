4c106faa2304353d737835122f0a358595c67692
ArtemD <artemdanilow95@gmail.com>
2023-08-10 17:29:52 +0200

    Add new arrow icon

--- src/app/components/svg/Icons.tsx
+++ src/app/components/svg/Icons.tsx
@@ -5,5 +5,15 @@ import Magnifier from "./icons/Magnifier";
 import Tiktok from "./icons/Tiktok";
 import Instagram from "./icons/Instagram";
 import Arrow from "./icons/Arrow";
+import LongArrow from "./icons/LongArrow";
 
-export { Sun, MenuButton, Cross, Magnifier, Tiktok, Instagram, Arrow };
+export {
+  Sun,
+  MenuButton,
+  Cross,
+  Magnifier,
+  Tiktok,
+  Instagram,
+  Arrow,
+  LongArrow,
+};
