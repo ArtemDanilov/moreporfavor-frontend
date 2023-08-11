1cd81877c3779773b9adf1da9f31e431a855c0a4
ArtemD <artemdanilow95@gmail.com>
2023-08-11 18:12:04 +0200

    Style 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import NotFound404 from "./components/svg/404";
+
 const notFound = () => {
-  return <div>Ups 404 Not found</div>;
+  return (
+    <div className="flex justify-center items-center mt-40 mb-20 lg:mt-52">
+      <div>
+        <NotFound404 className="w-[18.75rem] h-auto mx-auto mb-8 sm:w-[25rem] xl:w-[35rem]" />
+        <p className="font-sans font-normal text-base text-black text-center px-8 xl:text-xl">
+          Ups...wygląda na to że ta strona nie istnieje, <br />
+          wróć na{" "}
+          <Link href="/" className="text-mocha">
+            główną stronę
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default notFound;
