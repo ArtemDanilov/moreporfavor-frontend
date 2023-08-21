dcbfce6f8d234e44b1cc7a015af6fc6fcd1d1676
ArtemD <artemdanilow95@gmail.com>
2023-08-21 22:03:07 +0200

    Add condition on image into About component

--- src/app/components/sections/About.tsx
+++ src/app/components/sections/About.tsx
@@ -11,14 +11,16 @@ const About = ({ image, text, className }: Props) => {
   return (
     <figure className={`max-w-sm mx-auto text-center ${className}`}>
       <div className="w-60 min-w-[15rem] h-60 rounded-full mx-auto mb-4 overflow-hidden">
-        <Image
-          src={image.url}
-          alt={image.alt || "zdjęcie autora"}
-          width={240}
-          height={240}
-          loading="lazy"
-          className="w-full h-full object-cover"
-        />
+        {image && (
+          <Image
+            src={image.url}
+            alt={image.alt || "zdjęcie autora"}
+            width={240}
+            height={240}
+            loading="lazy"
+            className="w-full h-full object-cover"
+          />
+        )}
       </div>
 
       <figcaption className="font-sans text-base font-normal">
