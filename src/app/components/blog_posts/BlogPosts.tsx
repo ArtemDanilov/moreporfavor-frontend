e2c4fef1ef806c2105c66b0a3cc818300b2b57c5
ArtemDanilov <artemdanilow95@gmail.com>
2023-09-04 18:54:49 +0200

    Rebuild BlogPosts component

--- src/app/components/blog_posts/BlogPosts.tsx
+++ src/app/components/blog_posts/BlogPosts.tsx
@@ -1,102 +1,53 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-import { paginate } from "@/app/helpers/pagination";
-
-import { sortASC, sortDESC } from "@/app/helpers/sortUtilities";
-import { Article, Meta } from "@/app/ts/types";
+import { Meta } from "@/app/ts/types";
 
 import BlogPost from "./BlogPost";
-import Pagination from "../pagination/Pagination";
+import path from "path";
 
 type Props = {
-  posts: Article[];
-  direction?: "vertical" | "horizontal";
-  tagsCategory?: string;
-  tagName?: string;
-  sort?: "asc" | "desc";
-  count?: {
-    from: number;
-    to?: number;
-  };
-  pagination?: boolean;
-  entriesPerPage?: number;
+  entries: Meta[];
+  direction?: "horizontal" | "vertical";
 };
 
-const BlogPosts = ({
-  posts,
-  direction = "vertical",
-  tagsCategory,
-  tagName,
-  sort = "desc",
-  count = { from: 0 },
-  pagination = false,
-  entriesPerPage = 8,
-}: Props) => {
-  const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get("page")) || 1;
-
+const BlogPosts = ({ entries, direction }: Props) => {
   const horizontal = direction === "horizontal";
 
-  const filteredPostsByCategory =
-    tagsCategory && tagName
-      ? posts.filter((post) => post[tagsCategory]?.slug === tagName)
-      : posts;
-
-  let filteredPosts: Meta[] = filteredPostsByCategory;
-
-  if (sort === "desc") {
-    filteredPosts = sortDESC(filteredPosts).slice(count.from, count.to);
-  } else if (sort === "asc") {
-    filteredPosts = sortASC(filteredPosts).slice(count.from, count.to);
-  } else if (count) {
-    filteredPosts = filteredPosts.slice(count.from, count.to);
-  }
-
-  const paginatedPosts = paginate(filteredPosts, currentPage, entriesPerPage);
-
-  const totalPages = Math.ceil(filteredPosts.length / entriesPerPage);
-
-  const finalPosts = pagination ? paginatedPosts : filteredPosts;
-
   return (
-    <>
-      <ul
-        className={`flex flex-wrap gap-6 ${
-          horizontal ? "md:flex-nowrap md:flex-col" : "justify-center"
-        }`}
-      >
-        {finalPosts.map((post) => {
-          const {
-            id,
-            title,
-            image,
-            publishedAt,
-            short_description,
-            slug,
-            category,
-          } = post;
+    <ul
+      className={`flex flex-wrap gap-6 ${
+        horizontal ? "md:flex-nowrap md:flex-col" : "justify-center"
+      }`}
+    >
+      {entries.map((data) => {
+        const {
+          id,
+          title,
+          image,
+          publishedAt,
+          short_description,
+          slug,
+          category,
+          countries,
+        } = data;
+        const pathToArticle = `/${category}/${countries}/${slug}`;
 
-          return (
+        return (
+          <>
             <li key={id}>
               <BlogPost
                 title={title}
                 image={image}
-                link={`/${category.slug}/${slug}`}
+                link={pathToArticle}
                 category={category}
+                country={countries}
                 publishDate={publishedAt}
                 description={short_description}
-                direction={direction}
+                direction={"vertical"}
               />
             </li>
-          );
-        })}
-      </ul>
-
-      {pagination && totalPages > 1 && (
-        <Pagination pages={totalPages} currentPage={currentPage} />
-      )}
-    </>
+          </>
+        );
+      })}
+    </ul>
   );
 };
 
