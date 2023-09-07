5cc73e490c89dbeb3d4f5b4c7f76717c6b975267
ArtemDanilov <artemdanilow95@gmail.com>
2023-09-07 19:13:43 +0200

    Add extra conditions to Pagination

--- src/app/components/pagination/Pagination.tsx
+++ src/app/components/pagination/Pagination.tsx
@@ -13,39 +13,45 @@ type Props = {
 const Pagination = ({ pages }: Props) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get("page"));
+  const currentPage = Number(searchParams.get("page")) || 1;
 
   const pagesCount = Array.from({ length: pages }).map((_, i) => i + 1);
 
   const nextPage = currentPage === pages ? pages : currentPage + 1;
   const prevPage = currentPage === 1 ? 1 : currentPage - 1;
+  const isPrevActive = currentPage === 1;
+  const isNextActive = currentPage === pages;
 
   return (
     <div className="pagination mt-8 flex justify-center items-center lg:mt-20">
       <Link
         href={`${pathname}?page=${prevPage}`}
-        className={`prev ${currentPage === 1 && "disabled"}`}
+        className={`prev ${isPrevActive ? "disabled" : ""}`}
       >
         <LongArrow className="arrow rotate-180" />
       </Link>
       <div>
-        {pagesCount.map((page) => (
-          <Link
-            key={page}
-            href={`${pathname}?page=${page}`}
-            className={`font-display font-normal text-lg px-2 py-1 ${
-              currentPage === page
-                ? "text-gray-200 pointer-events-none"
-                : "text-green"
-            }`}
-          >
-            {page}
-          </Link>
-        ))}
+        {pagesCount.map((page) => {
+          const isPageActive = currentPage === page;
+
+          return (
+            <Link
+              key={page}
+              href={`${pathname}?page=${page}`}
+              className={`font-display font-normal text-lg px-2 py-1 ${
+                isPageActive
+                  ? "text-gray-200 pointer-events-none"
+                  : "text-green"
+              }`}
+            >
+              {page}
+            </Link>
+          );
+        })}
       </div>
       <Link
         href={`${pathname}?page=${nextPage}`}
-        className={`next ${currentPage === pages && "disabled"}`}
+        className={`next ${isNextActive ? "disabled" : ""}`}
       >
         <LongArrow className="arrow" />
       </Link>
