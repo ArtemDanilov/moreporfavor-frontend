4fd70eafd3dc01fecaba0ac85469b8fcbf976df8
ArtemDanilov <artemdanilow95@gmail.com>
2023-10-05 15:47:04 +0200

    Create "all articles" page

---
+++ src/app/(pages)/wszystkie-artykuly/page.tsx
@@ -0,0 +1,54 @@
+import Section from "../../components/Section";
+import Hero from "../../components/hero/category/Hero";
+import Collection from "../../helpers/collection";
+import Pagination from "../../components/pagination/Pagination";
+import BlogPosts from "../../components/blog_posts/BlogPosts";
+import Wrapper from "@/app/Wrapper";
+
+// import { Metadata } from "next";
+
+export const dynamic = "force-dynamic";
+
+const CategoryPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  const currentPage = searchParams["page"] ?? "1";
+
+  const collection = await Collection({
+    collection: "articles",
+    pagination: true,
+    entriesPerPage: 9,
+    currentPage: Number(currentPage),
+  });
+
+  if (!collection) {
+    return (
+      <>
+        <Hero title="Wszystkie artykuły" />
+        <p className="text-center">Coś poszło nie tak z pobraniem treści</p>
+      </>
+    );
+  }
+
+  const { totalPages } = collection?.attributes;
+
+  return (
+    <Wrapper>
+      <>
+        <Hero title="Wszystkie artykuły" />
+
+        {collection.entries.length > 0 && (
+          <Section>
+            <BlogPosts entries={collection.entries} />
+
+            {totalPages > 1 && <Pagination pages={totalPages} />}
+          </Section>
+        )}
+      </>
+    </Wrapper>
+  );
+};
+
+export default CategoryPage;
