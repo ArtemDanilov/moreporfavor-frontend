a0f057ceea019d4ef7ff8076dd24676e3c96e37f
ArtemDanilov <artemdanilow95@gmail.com>
2023-10-26 10:36:35 +0200

    Create About me content

--- src/app/(pages)/o-mnie/page.tsx
+++ src/app/(pages)/o-mnie/page.tsx
@@ -1,5 +1,9 @@
 import { Metadata } from "next";
 import Wrapper from "@/app/Wrapper";
+import Image from "next/image";
+import Section from "@/app/components/Section";
+import Hero from "@/app/components/hero/category/Hero";
+import Shapes from "@/app/components/svg/shapes";
 
 export const metadata: Metadata = {
   title: "O mnie",
@@ -10,7 +14,31 @@ export const metadata: Metadata = {
 const About: React.FC = () => {
   return (
     <Wrapper>
-      <h1>O mnie</h1>
+      <>
+        <Hero title="O mnie" />
+
+        <Section className="relative max-w-3xl overflow-x-hidden md:overflow-visible xl:max-w-4xl">
+          <div className="flex gap-4 flex-col items-center md:flex-row">
+            <Image
+              src="/assets/Kaja.jpg"
+              alt="zdjęcie autora"
+              width={300}
+              height={480}
+              className="w-full h-full max-w-md object-cover rounded-xl"
+            />
+            <Shapes className="absolute bottom-0 -z-10 w-[34rem] h-[43.5rem] text-[#FFCD60] md:-bottom-24 md:left-32" />
+            <p className="font-sans font-normal text-base md:text-lg">
+              Hej, jestem Kaja! Kocham słońce, dobre jedzenie, ładne budynki i
+              odkrywanie nowych miejsc. Blog moreporfavor powstał, by dzielić
+              się podróżniczą pasją i utrwalać wspomnienia. Nazwa nie jest
+              przypadkowa. Ciągle mam ochotę na “more”. “Por favor” nawiązuje do
+              języka hiszpańskiego, którego się uczę, a pierwszy kontakt z nim
+              miałam, pracując na jednej z Wysp Kanaryjskich po maturze. Mam
+              nadzieję, że zostaniesz tu na dłużej!
+            </p>
+          </div>
+        </Section>
+      </>
     </Wrapper>
   );
 };
