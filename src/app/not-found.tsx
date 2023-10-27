9362cfa9ba6108ab364a1b75dacbf17eac0b220c
ArtemDanilov <artemdanilow95@gmail.com>
2023-10-27 13:06:50 +0200

    Change 'a' tag to 'LInk' in not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 import NotFound404 from "./components/svg/404";
 
 export const metadata: Metadata = {
@@ -13,13 +14,10 @@ const NotFound = () => {
         <NotFound404 className="w-[18.75rem] h-auto mx-auto mb-8 sm:w-[25rem] xl:w-[35rem]" />
         <p className="font-sans font-normal text-base text-black text-center px-8 xl:text-xl">
           Ups...wygląda na to że ta strona nie istnieje, <br />
-          wróć na{" "}
-          {/* Next Link is doesn't work at the moment on 404 page.
-              You can apply Link component when bug will be fixed.
-          */}
-          <a href="/" className="text-mocha">
+          wróć na
+          <Link href="/" className="text-mocha">
             główną stronę
-          </a>
+          </Link>
         </p>
       </div>
     </div>
